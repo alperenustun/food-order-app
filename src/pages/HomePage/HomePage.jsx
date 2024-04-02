43f84325f1f6411481d22c5b42e72eed2a7bbcde
Altuğ Altuner <vefgend@gmail.com>
2024-04-02 16:16:28 +0300

    Fonts of texts are fixed, colors of customer infos added

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,15 +1,61 @@
 import Sidebar from "@/components/Sidebar/Sidebar";
 import "./HomePage.scss";
 import { useAuth } from "../../components/AuthProvider";
+import OrderReports from "../OrderReports/OrderReports";
+
+import avatar1 from "../../assets/avatar1.png";
+import avatar2 from "../../assets/avatar2.png";
+import avatar3 from "../../assets/avatar3.png";
+import avatar4 from "../../assets/avatar4.png";
+import avatar5 from "../../assets/avatar5.png";
+
+const orders = [
+  // ...sipariş verileriniz burada olmalı...
+  {
+    customerName: "Eren Jaegar",
+    customerAvatar: avatar1,
+    menu: "Spicy seasoned seafood noodles ",
+    totalPayment: 125,
+    status: "Completed",
+  },
+  {
+    customerName: "Reiner Braunn",
+    customerAvatar: avatar2,
+    menu: "Salted Pasta with mushroom sauce",
+    totalPayment: 145,
+    status: "Preparing",
+  },
+  {
+    customerName: "Levi Ackerman",
+    customerAvatar: avatar3,
+    menu: "Beef dumpling in hot and sour soup",
+    totalPayment: 105,
+    status: "Pending",
+  },
+  {
+    customerName: "Historia Reiss",
+    customerAvatar: avatar4,
+    menu: "Hot spicy fried rice with omelet",
+    totalPayment: 45,
+    status: "Completed",
+  },
+  {
+    customerName: "Hanji Zoe",
+    customerAvatar: avatar5,
+    menu: "Sichuan hot pot mixed seafood",
+    totalPayment: 245,
+    status: "Completed",
+  },
+];
 
 function HomePage() {
   const auth = useAuth(); // auth'u const {fireStoreUser} = useAuth() şeklinde alırsanız user bilgilerine ulaşabilirsiniz
   return (
     <div>
-      <Sidebar />
+      <OrderReports orders={orders} />
+      {/* <Sidebar />
       <div className="mainRoot">
-        <h1>Homepage example</h1>
-      </div>
+      </div> */}
     </div>
   );
 }
--- src/pages/OrderReports/OrderReports.jsx
+++ src/pages/OrderReports/OrderReports.jsx
@@ -1,1 +1,51 @@
-asasasa
+import { useState } from "react";
+import "./OrderReports.scss";
+import filterPhoto from "../../assets/Filter.png";
+
+const OrderReports = ({ orders }) => {
+
+    const statusClass = (status) => {
+        return status.toLowerCase().replace(" ", "");
+    };
+
+
+    return (
+        <div className="order-report-container">
+            <div className="order-report-header">
+                <h1>Order Report</h1>
+                <button className="filter-order-btn"><img src={filterPhoto} alt="filter-photo" /> Filter Order</button>
+            </div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Customer</th>
+                        <th>Menu</th>
+                        <th>Total Payment</th>
+                        <th>Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {orders.map((order, index) => (
+                        <tr key={index}>
+                            <td>
+                                <div className="customer-info">
+                                    <img src={order.customerAvatar} alt={order.customerName} />
+                                    {order.customerName}
+                                </div>
+                            </td>
+                            <td>{order.menu}</td>
+                            <td>${order.totalPayment}</td>
+                            <td>
+                                <div className={`status ${statusClass(order.status)}`}>
+                                    <span className="status-opacitylow">{order.status}</span>
+                                </div>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default OrderReports;
