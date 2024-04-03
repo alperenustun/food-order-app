6504848a6492abbdfcce7e03eeab7a8c98ada417
Altuğ Altuner <vefgend@gmail.com>
2024-04-04 02:14:22 +0300

    Listing and showing orders is done

--- src/pages/OrderReports/OrderReports.jsx
+++ src/pages/OrderReports/OrderReports.jsx
@@ -4,19 +4,54 @@ import filterPhoto from "../../assets/Filter.png";
 
 const OrderReports = ({ orders }) => {
 
+    const [sortedOrders, setSortedOrders] = useState(orders);
+
     const statusClass = (status) => {
         return status.toLowerCase().replace(" ", "");
     };
+
+    const handleFilterChange = (e) => {
+        const filter = e.target.value;
+
+        let sortedOrders;
+        switch (filter) {
+            case "highest":
+                sortedOrders = [...orders].sort((a, b) => b.totalPayment - a.totalPayment);
+                break;
+            case "lowest":
+                sortedOrders = [...orders].sort((a, b) => a.totalPayment - b.totalPayment);
+                break;
+            case "status":
+                const statusPriority = { Completed: 1, Preparing: 2, Pending: 3 };
+                sortedOrders = [...orders].sort((a, b) => {
+                    return statusPriority[a.status] - statusPriority[b.status];
+                });
+                break;
+            default:
+                sortedOrders = [...orders];
+        }
+
+        setSortedOrders(sortedOrders);
+    };
+
 
 
     return (
         <div className="order-report-container">
             <div className="order-report-header">
                 <h1>Order Report</h1>
-                <button className="filter-order-btn"><img src={filterPhoto} alt="filter-photo" /> Filter Order</button>
+                <div className="filter-order-btn">
+                    <img src={filterPhoto} alt="filter-icon" />
+                    <select onChange={handleFilterChange} className="filter-select">
+                        <option value="">Filter Orders</option>
+                        <option value="highest">Highest Payment</option>
+                        <option value="lowest">Lowest Payment</option>
+                        <option value="status">Status</option>
+                    </select>
+                </div>
             </div>
             <table>
-                <thead>
+                <thead className="border-line-order-report">
                     <tr>
                         <th>Customer</th>
                         <th>Menu</th>
@@ -25,7 +60,7 @@ const OrderReports = ({ orders }) => {
                     </tr>
                 </thead>
                 <tbody>
-                    {orders.map((order, index) => (
+                    {sortedOrders.map((order, index) => (
                         <tr key={index}>
                             <td>
                                 <div className="customer-info">
