609e4ccba2ef35b1c3cc74d638e9b612084307ce
Altuğ Altuner <vefgend@gmail.com>
2024-04-04 18:12:44 +0300

    FO-11- sidebar settings code implementation is done

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,13 +2,15 @@ import Sidebar from "@/components/Sidebar/Sidebar";
 import "./HomePage.scss";
 import { useAuth } from "../../components/AuthProvider";
 
+import SettingsSidebar from "../SettingsSidebar/SettingsSidebar";
+
 function HomePage() {
   const auth = useAuth(); // auth'u const {fireStoreUser} = useAuth() şeklinde alırsanız user bilgilerine ulaşabilirsiniz
   return (
     <div>
-      <Sidebar />
+      {/* <Sidebar /> */}
       <div className="mainRoot">
-        <h1>Homepage example</h1>
+        <SettingsSidebar />
       </div>
     </div>
   );
