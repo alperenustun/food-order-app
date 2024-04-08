9dfe178e27e881d7e77c45eacded88afe782e3ae
Samet Can Gözüaçık <sametcanngozuacik@gmail.com>
2024-04-08 16:15:37 +0300

    [FO-6] Button part has been completed

--- src/components/PaymentConfirmation/PaymentConfirmation.jsx
+++ src/components/PaymentConfirmation/PaymentConfirmation.jsx
@@ -4,10 +4,23 @@ import "./PaymentConfirmation.scss";
 
 
 function PaymentConfirmation() {
-    return <div>
-     
+    return <div className="PaymentConfirmation">
+        <div className="PaymentConfirmation-box">
+            <div className="PaymentConfirmation-Button-Box">
+                <button className="PaymentConfirmation-Button">Dine In</button>
+                <button className="PaymentConfirmation-Button">To Go</button>
+                <button className="PaymentConfirmation-Button">Delivery</button>
+            </div>
+            <div className="PaymentConfirmationtitle">
+                <h3>Item</h3>
+                <div>
+                <h3>Qty</h3>
+                <h3>Price</h3>
+                </div>
+            </div>
+       </div>
     </div>
-    
+
 }
 
 export default PaymentConfirmation;
