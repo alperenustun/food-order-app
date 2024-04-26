b95146b706efa5c595e0104dd0ff77fbf1253065
yayakup <yayakup66@gmail.com>
2024-04-26 15:16:03 +0300

    [sccs isimleri,export ve klasor adi, genel kod yapisi duzeltildi.]

--- src/components/DashboardAnalyticsHeader/DashboardAnalyticsHeader.jsx
+++ src/components/DashboardAnalyticsHeader/DashboardAnalyticsHeader.jsx
@@ -1,55 +1,71 @@
 import { useState } from "react";
 import "./DashboardAnalyticsHeader.scss";
-import dashboardImage1 from "./../../assets/dashboard-image/frame-up.png";
-import dashboardImage2 from "./../../assets/dashboard-image/coin.png";
-import dashboardImage3 from "./../../assets/dashboard-image/icon.png";
-import dashboardImage4 from "./../../assets/dashboard-image/frame-68.png";
-import dashboardImage5 from "./../../assets/dashboard-image/customer.png";
+import dashboardImage1 from "@/assets/dashboard-image/frame-up.png";
+import dashboardImage2 from "@/assets/dashboard-image/coin.png";
+import dashboardImage3 from "@/assets/dashboard-image/order.png";
+import dashboardImage4 from "@/assets/dashboard-image/frame-68.png";
+import dashboardImage5 from "@/assets/dashboard-image/customer.png";
 
-function DashboardAnalytics() {
+function DashboardAnalyticsHeader() {
   return (
     <div className="dashboard-container">
       <div className="total-revenue box">
         <div className="dashboard-header">
-          <img src={dashboardImage2} alt="" />
-          <div>
-            <p>+32.40%</p>
-            <img src={dashboardImage1} alt="" />
+          <img
+            className="dashboard-header-image"
+            src={dashboardImage2}
+            alt=""
+          />
+          <div className="dashboard-header-status">
+            <p className="dashboard-header-status-p">+32.40%</p>
+            <img
+              className="dashboard-header-status-image"
+              src={dashboardImage1}
+              alt=""
+            />
           </div>
         </div>
         <div className="dashboard-content">
-          <h5>$10,243.00</h5>
-          <p>Total Revenue</p>
+          <h5 className="dashboard-content-h5">$10,243.00</h5>
+          <p className="dashboard-content-p">Total Revenue</p>
         </div>
       </div>
       <div className="Total-dish-ordered box">
         <div className="dashboard-header">
-          <img src={dashboardImage3} alt="" />
-          <div className="statistical-decline">
-            <p>-12.40%</p>
+          <img
+            className="dashboard-header-image"
+            src={dashboardImage3}
+            alt=""
+          />
+          <div className="dashboard-header-status">
+            <p className="statistical-decline-p">-12.40%</p>
             <img src={dashboardImage4} alt="" />
           </div>
         </div>
         <div className="dashboard-content">
-          <h5>23,456</h5>
-          <p>Total Dish Ordered</p>
+          <h5 className="dashboard-content-h5">23,456</h5>
+          <p className="dashboard-content-p">Total Dish Ordered</p>
         </div>
       </div>
       <div className="Total-customer box">
         <div className="dashboard-header">
-          <img src={dashboardImage5} alt="" />
-          <div>
-            <p>+2.40%</p>
+          <img
+            className="dashboard-header-image"
+            src={dashboardImage5}
+            alt=""
+          />
+          <div className="dashboard-header-status">
+            <p className="dashboard-header-status-p">+2.40%</p>
             <img src={dashboardImage1} alt="" />
           </div>
         </div>
         <div className="dashboard-content">
-          <h5>1,234</h5>
-          <p>Total Customer</p>
+          <h5 className="dashboard-content-h5">1,234</h5>
+          <p className="dashboard-content-p">Total Customer</p>
         </div>
       </div>
     </div>
   );
 }
 
-export default DashboardAnalytics;
+export default DashboardAnalyticsHeader;
