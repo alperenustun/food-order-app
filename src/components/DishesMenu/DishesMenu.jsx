ad53d67e53ee3874f120c5c91dd341e4f56d81a7
Ali Çalışkan <caliskanali5534@gmail.com>
2024-04-29 13:49:29 +0300

    [FO-10] scss jsx completed

--- src/components/DishesMenu/DishesMenu.jsx
+++ src/components/DishesMenu/DishesMenu.jsx
@@ -1,52 +1,67 @@
 import React from "react";
 import "./DishesMenu.scss";
+import spicyImage from "@/assets/images/spicy.png";
+import saltedImage from "@/assets/images/salted.svg";
+import beefImage from "@/assets/images/foo.png";
+import healtImage from "@/assets/images/healt.png";
+import instImage from "@/assets/images/instant.png";
+import seeImage from "@/assets/images/seafood.png";
 
 function DishesMenu() {
     const tabs = [
         {
             id: 1,
-            Image:"spicy.png",
+            image: spicyImage,
             description: "Spicy seasoned seafood noodles",
             price: "$ 2.29",
-            
+
+
         },
         {
             id: 2,
-            description: "Spicy seasoned seafood noodles",
+            image: saltedImage,
+            description: "Salted pasta with mushroom sauce",
             price: "$ 2.29",
         },
         {
             id: 3,
-            description: "Spicy seasoned seafood noodles",
+            image: beefImage,
+            description: "Beef dumpling in hot and sour soup",
             price: "$ 2.29",
         },
         {
             id: 4,
-            description: "Spicy seasoned seafood noodles",
+            image: healtImage,
+            description: "Healthy noodle with spinach leaf",
             price: "$ 2.29",
         },
         {
             id: 5,
+            image: healtImage,
             description: "Spicy seasoned seafood noodles",
             price: "$ 2.29",
         },
         {
             id: 6,
+            image: instImage,
             description: "Spicy seasoned seafood noodles",
             price: "$ 2.29",
         },
         {
             id: 7,
+            image: seeImage,
             description: "Spicy seasoned seafood noodles",
             price: "$ 2.29",
         },
         {
             id: 8,
+            image: saltedImage,
             description: "Spicy seasoned seafood noodles",
             price: "$ 2.29",
         },
         {
             id: 9,
+            image: instImage,
             description: "Spicy seasoned seafood noodles",
             price: "$ 2.29",
         },
@@ -54,16 +69,19 @@ function DishesMenu() {
 
     return (
         <div>
-            <select className="btn-option" name="" id="">
-                <option value="dineın">Dine In</option>
-                <option value="Togo">To go</option>
-                <option value="Delivery">Delivery</option>
-            </select>
+            <div className="title">
+                <h3>Choose Dishes</h3>
+                <select className="btn-option" name="" id="">
+                    <option value="dineın">Dine In</option>
+                    <option value="Togo">To go</option>
+                    <option value="Delivery">Delivery</option>
+                </select>
+            </div>
             <ul className="boxes">
                 {tabs.map((tab) => (
                     <li key={tab.id} className="box">
                         <div className="box-content">
-                        <img src={tab.image} className="box-image" />
+                            <img src={tab.image} className="box-image" />
                             <div className="description">{tab.description}</div>
                             <div className="price">{tab.price}</div>
                         </div>
