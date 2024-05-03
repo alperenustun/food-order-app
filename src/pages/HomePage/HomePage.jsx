fe21e66a10ef02e745e0bce1afd3fbe223dadc0e
Altuğ Altuner <vefgend@gmail.com>
2024-05-03 15:33:49 +0300

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,15 +1,15 @@
-import React, { useState } from "react";
 import Sidebar from "@/components/Sidebar/Sidebar";
 import "./HomePage.scss";
 import { useAuth } from "../../components/AuthProvider";
-import MostOrdered from "../../components/MostOrdered/MostOrdered";
 
 function HomePage() {
   const auth = useAuth(); // auth'u const {fireStoreUser} = useAuth() şeklinde alırsanız user bilgilerine ulaşabilirsiniz
   return (
     <div>
+
       <Sidebar />
       <div className="mainRoot">
+
       </div>
     </div>
   );
