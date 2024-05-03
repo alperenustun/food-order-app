dea550ad9f019bdf5872c5ceb2f14a57fdaba0dc
Ali Çalışkan <caliskanali5534@gmail.com>
2024-05-03 10:20:33 +0300

    [FO]10 mission completed

--- src/components/DishesMenu/DishesMenu.jsx
+++ src/components/DishesMenu/DishesMenu.jsx
@@ -4,21 +4,21 @@ function DishesMenu({ filteredDishes }) {
 
     return (
         <div>
-            <div className="title">
+            <div className="dishes-container">
                 <h3>Choose Dishes</h3>
-                <select className="btn-option" name="" id="">
+                <select className="dishes-btn-select" name="" id="">
                     <option value="dineın">Dine In</option>
                     <option value="Togo">To go</option>
                     <option value="Delivery">Delivery</option>
                 </select>
             </div>
-            <ul className="boxes">
+            <ul className="dishes-boxes">
                 {filteredDishes.map((tab) => (
-                    <li key={tab.id} className="box">
-                        <div className="box-content">
-                            <img src={tab.image} className="box-image" />
-                            <div className="description">{tab.description}</div>
-                            <div className="price">{tab.price}</div>
+                    <li key={tab.id} className="dishes-box">
+                        <div className="dishes-box-content">
+                            <img src={tab.image} className="dishes-box-image" />
+                            <div className="dishes-description">{tab.description}</div>
+                            <div className="dishes-price">{tab.price}</div>
                         </div>
                     </li>
                 ))}
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,7 +3,7 @@ import "./HomePage.scss";
 import { useAuth } from "../../components/AuthProvider";
 import DishesMenu from "@/components/DishesMenu/DishesMenu";
 import { useState } from "react";
-import { foods } from "../../assets/data/foods";
+import { foods } from "../../assets/db/foods";
 
 function HomePage() {
   const auth = useAuth(); // auth'u const {fireStoreUser} = useAuth() şeklinde alırsanız user bilgilerine ulaşabilirsiniz
