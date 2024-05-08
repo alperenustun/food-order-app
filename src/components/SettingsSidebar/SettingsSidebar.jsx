476fee1075c87e0b8ed45d002232ec52e9588388
Altuğ Altuner <vefgend@gmail.com>
2024-05-08 12:55:44 +0300

    instead of onclick, i used navlink, scss is edited

--- src/components/SettingsSidebar/SettingsSidebar.jsx
+++ src/components/SettingsSidebar/SettingsSidebar.jsx
@@ -1,68 +1,76 @@
-import "./SettingsSidebar.scss";
+import "@/components/SettingsSidebar/SettingsSidebar.scss";
+import { NavLink } from "react-router-dom";
+const homeImg = "/src/assets/home.png";
+const likeImg = "/src/assets/like.png";
+const foImg = "/src/assets/foImg.png";
+const notificationImg = "/src/assets/notification.png";
+const lockImg = "/src/assets/lock.png";
+const settingImg = "/src/assets/setting.png";
 
 function SettingsSidebar() {
-
-    const homeImg = "/src/assets/home.png";
-    const likeImg = "/src/assets/like.png";
-    const foImg = "/src/assets/foImg.png";
-    const notificationImg = "/src/assets/notification.png";
-    const lockImg = "/src/assets/lock.png";
-    const settingImg = "/src/assets/setting.png";
-
-    function makeActiveDiv(e) {
-        e.stopPropagation();
-
-        const targetDiv = e.target.closest('.sidebar-div');
-
-        if (targetDiv) {
-            const allDivs = document.querySelectorAll('.sidebar-div');
-            allDivs.forEach(div => {
-                div.style.backgroundColor = '#1F1D2B';
-                div.querySelector('h3').style.color = '#fff';
-            });
-
-            targetDiv.style.backgroundColor = '#EA7C6942';
-            targetDiv.querySelector('h3').style.color = '#EA7C69';
-        }
-    }
 
     return (
         <div className="settings-sidebar-main">
 
-            <button className="sidebar-div" onClick={makeActiveDiv} id="appearance-sidebar-id" tabIndex={0}>
-                <h3> <img src={likeImg} alt="" srcSet="" />Appearance</h3>
-                <p>Dark and Light mode, Font size</p>
-            </button>
+            <NavLink to="/settings/appearance" id="appearance-nav-id" className={(object) => {
+                return object.isActive ? "sidebar-div-active" : "sidebar-div";
+            }} >
+                <button className="sidebar-btn-class" id="appearance-sidebar-id" >
+                    <h3 className="sidebar-header"> <img className="settings-sidebar-img" src={likeImg} alt="" srcSet="" />Appearance</h3>
+                    <p className="settings-sidebar-title">Dark and Light mode, Font size</p>
+                </button>
+            </NavLink>
 
-            <button className="sidebar-div" onClick={makeActiveDiv} id="restaurant-sidebar-id">
-                <h3><img src={homeImg} alt="" srcSet="" />Your Restaurant</h3>
-                <p>Find the Nearest Restaurant details</p>
-            </button>
+            <NavLink to="/settings/restaurant" id="restaurant-nav-id" className={(object) => {
+                return object.isActive ? "sidebar-div-active" : "sidebar-div";
+            }} >
+                <button className="sidebar-btn-class" id="restaurant-sidebar-id" >
+                    <h3 className="sidebar-header"><img className="settings-sidebar-img" src={homeImg} alt="" srcSet="" />Your Restaurant</h3>
+                    <p className="settings-sidebar-title">Dark and Light mode, Font size</p>
+                </button>
+            </NavLink>
 
-            <button className="sidebar-div" onClick={makeActiveDiv} id="product-sidebar-id">
-                <h3><img src={settingImg} alt="" srcSet="" />Product Management</h3>
-                <p>Manage your product, pricing, etc</p>
-            </button>
+            <NavLink to="/settings/products" id="products-nav-id" className={(object) => {
+                return object.isActive ? "sidebar-div-active" : "sidebar-div";
+            }} >
+                <button className="sidebar-btn-class" id="products-sidebar-id">
+                    <h3 className="sidebar-header"><img className="settings-sidebar-img" src={settingImg} alt="" srcSet="" />Product Management</h3>
+                    <p className="settings-sidebar-title">Manage your product, pricing, etc</p>
+                </button>
+            </NavLink>
 
-            <button className="sidebar-div" onClick={makeActiveDiv} id="notifications-sidebar-id">
-                <h3><img src={notificationImg} alt="" srcSet="" />Notifications</h3>
-                <p>Customize your notifications</p>
-            </button>
-
-            <button className="sidebar-div" onClick={makeActiveDiv} id="security-sidebar-id">
-                <h3><img src={lockImg} alt="" srcSet="" />Security</h3>
-                <p>Configure Password, PIN, etc</p>
-            </button>
-
-            <button className="sidebar-div" onClick={makeActiveDiv} id="configure-sidebar-id">
-                <h3><img src={settingImg} alt="" srcSet="" />Configure</h3>
-                <p>Display new features</p>
-            </button>
-
-            <button className="sidebar-div" onClick={makeActiveDiv} id="about-us-sidebar-id">
-                <h3><img src={foImg} alt="" srcSet="" />About Us</h3>
-                <p>Find out more about Posly</p>
-            </button>
+            <NavLink to="/settings/notifications" id="notifications-nav-id" className={(object) => {
+                return object.isActive ? "sidebar-div-active" : "sidebar-div";
+            }} >
+                <button className="sidebar-btn-class" id="notifications-sidebar-id">
+                    <h3 className="sidebar-header"><img className="settings-sidebar-img" src={notificationImg} alt="" srcSet="" />Notifications</h3>
+                    <p className="settings-sidebar-title">Customize your notifications</p>
+                </button>
+            </NavLink>
+            <NavLink to="/settings/security" id="security-nav-id" className={(object) => {
+                return object.isActive ? "sidebar-div-active" : "sidebar-div";
+            }} >
+                <button className="sidebar-btn-class" id="security-sidebar-id">
+                    <h3 className="sidebar-header"><img className="settings-sidebar-img" src={lockImg} alt="" srcSet="" />Security</h3>
+                    <p className="settings-sidebar-title">Configure Password, PIN, etc</p>
+                </button>
+            </NavLink>
+            <NavLink to="/settings/configure" id="configure-nav-id" className={(object) => {
+                return object.isActive ? "sidebar-div-active" : "sidebar-div";
+            }} >
+                <button className="sidebar-btn-class" id="configure-sidebar-id">
+                    <h3 className="sidebar-header"><img className="settings-sidebar-img" src={settingImg} alt="" srcSet="" />Configure</h3>
+                    <p className="settings-sidebar-title">Display new features</p>
+                </button>
+            </NavLink>
+            <NavLink to="/settings/about-us" id="about-nav-id" className={(object) => {
+                return object.isActive ? "sidebar-div-active" : "sidebar-div";
+            }} >
+                <button className="sidebar-btn-class" id="about-us-sidebar-id">
+                    <h3 className="sidebar-header"><img className="settings-sidebar-img" src={foImg} alt="" srcSet="" />About Me</h3>
+                    <p className="settings-sidebar-title">Find out more about me</p>
+                </button>
+            </NavLink>
         </div>
     );
 };
