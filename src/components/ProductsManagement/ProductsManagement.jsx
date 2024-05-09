214fc34a41c0d30320d5cf294f519d21c02de692
Samet Can Gözüaçık <sametcanngozuacik@gmail.com>
2024-05-09 16:58:26 +0300

    [FO-12] produckts-management-button-box eklendi

--- src/components/ProductsManagement/ProductsManagement.jsx
+++ src/components/ProductsManagement/ProductsManagement.jsx
@@ -5,9 +5,25 @@ import "./ProductsManagement.scss";
 function ProductsManagement() {
     return (
         <div className="produckts-management">
-            <div className="produckts-management-title-box" >
-                <h1 className="produckts-management-title3">Products Management</h1>
-                <button className="produckts-management-title-box-btn"> Manage Categories</button>
+            <div className="produckts-management-title-section" >
+                <div className="produckts-management-title-box">
+                    <h1 className="produckts-management-title3">Products Management</h1>
+                    <button className="produckts-management-title-box-btn"><img className="" src="public/Option.png" alt="icon" /> Manage Categories</button>
+                </div>
+                <div>
+                    <ul className="produckts-management-link-box">
+                        <li><a href="#">Hot Dishes</a></li>
+                        <li><a href="#">Cold Dishes</a></li>
+                        <li><a href="#">Soup</a></li>
+                        <li><a href="#">Grill</a></li>
+                        <li><a href="#">Appetizer</a></li>
+                        <li><a href="#">Dessert</a></li>
+                    </ul>
+                </div>
+                <div className="produckts-management-button-box">
+                    <button className="produckts-management-button-box-btn">Discard Changes</button>
+                    <button className="produckts-management-button-box-btn">Save Changes</button>
+                </div>
             </div>
         </div>
 
