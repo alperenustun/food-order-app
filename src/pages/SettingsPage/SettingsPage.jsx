77373544c9db1b8fe895fd9fd1a09ce4ae850237
Altuğ Altuner <vefgend@gmail.com>
2024-05-10 12:44:03 +0300

    scss is fixing

--- src/pages/SettingsPage/SettingsPage.jsx
+++ src/pages/SettingsPage/SettingsPage.jsx
@@ -1,15 +1,13 @@
 import "../SettingsPage/SettingsPage.scss";
-import SettingsSidebar from "../../components/SettingsSidebar/SettingsSidebar";
+import SettingsSidebar from "@/components/SettingsSidebar/SettingsSidebar";
 import Sidebar from "@/components/Sidebar/Sidebar";
 
 function SettingsPage() {
 
   return (
-    <main className="settings-page">
+    <main className="mainRoot">
       <Sidebar />
-      <div className="set-product-div">
-        <SettingsSidebar />
-      </div>
+      <SettingsSidebar />
     </main>
   );
 }
