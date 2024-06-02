0a4a76390feb6c156a63969bbc713f19027e785a
yayakup <yayakup66@gmail.com>
2024-06-03 00:11:34 +0300

    [dashboard sayfa duzenlemesi]

--- src/components/DishesMenu/DishesMenu.jsx
+++ src/components/DishesMenu/DishesMenu.jsx
@@ -1,23 +1,31 @@
 import "./DishesMenu.scss";
-import { useState } from "react";
+import PropTypes from "prop-types";
 
+function DishesMenu({ filteredDishes, setDishInfo }) {
+  
+  if (!filteredDishes) {
+    return <div>Loading...</div>;
+  }
 
-function DishesMenu({ filteredDishes,setDishInfo }) {
+  if (filteredDishes.length === 0) {
+    return <div>No dishes available</div>;
+  }
+
   return (
     <div>
       <div className="dishes-container">
         <h3>Choose Dishes</h3>
         <select className="dishes-btn-select" name="" id="">
-          <option value="dineın">Dine In</option>
-          <option value="Togo">To go</option>
-          <option value="Delivery">Delivery</option>
+          <option value="dinein">Dine In</option>
+          <option value="togo">To go</option>
+          <option value="delivery">Delivery</option>
         </select>
       </div>
       <ul className="dishes-boxes">
         {filteredDishes.map((tab) => (
           <li onClick={() => setDishInfo((oldTab) => [...oldTab, tab])} key={tab.id} className="dishes-box">
             <div className="dishes-box-content">
-              <img src={tab.image} className="dishes-box-image" />
+              <img src={tab.image} className="dishes-box-image" alt={tab.name} />
               <div className="dishes-description">{tab.description}</div>
               <div className="dishes-price">{tab.price}</div>
             </div>
@@ -27,5 +35,10 @@ function DishesMenu({ filteredDishes,setDishInfo }) {
     </div>
   );
 }
+
+DishesMenu.propTypes = {
+  filteredDishes: PropTypes.array.isRequired,
+  setDishInfo: PropTypes.func.isRequired,
+};
 
 export default DishesMenu;
